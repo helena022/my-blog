f1d295841812761a86b0dc848fe8b6ecdc310dd7
Helena Viher <viherhelena@gmail.com>
2022-04-28 13:10:23 +0200

    development: add return types to functions

--- src/utils/validations.tsx
+++ src/utils/validations.tsx
@@ -1,14 +1,15 @@
-export const hasValue = (value: string | number) => value !== '';
+export const hasValue = (value: string | number): boolean => value !== '';
 
-export const isBetween = (length: number, min: number, max: number) => length < min || length > max;
+export const isBetween = (length: number, min: number, max: number): boolean =>
+  length < min || length > max;
 
-export const isEmailValid = (email: string) => {
+export const isEmailValid = (email: string): boolean => {
   const re =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
 };
 
-export const isPasswordSecure = (password: string) => {
+export const isPasswordSecure = (password: string): boolean => {
   const re = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})');
   return re.test(password);
 };
