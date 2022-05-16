0c62b2156b40c123de34204e3a75175c89dfebcc
Helena Viher <viherhelena@gmail.com>
2022-05-16 20:42:39 +0200

    development: add my blog screen

--- src/screens/MyBlogScreen.tsx
+++ src/screens/MyBlogScreen.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, ScrollView } from 'react-native';
+import { useAuth } from '../contexts/AuthContext';
+import FullScreenSpinner from '../components/FullScreenSpinner';
+import Blog from '../components/Blog';
+import { myBlog } from '../styles/myBlog';
 
 const MyBlogScreen = () => {
+  const { user } = useAuth();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [authorId, setAuthorId] = useState<null | number>(null);
   return (
-    <View>
-      <Text>My Blog Screen</Text>
-    </View>
+    <ScrollView contentContainerStyle={myBlog.screenContainer}>
+      {isLoading && <FullScreenSpinner />}
+      <View style={myBlog.blogContainer}>
+        <Blog authorId={user.id} />
+      </View>
+    </ScrollView>
   );
 };
 
